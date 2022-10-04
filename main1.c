d1ebf46c3c4a2e30f4674879d8a8463ee408b500
강승원 <kang574534@gmail.com>
2022-10-04 16:09:44 +0900

    Create main1.c
    2

---
+++ main1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+	
+	printf("정수 하나를 입력하시오:");
+	scanf("%i",&i);
+	
+	if(i>0)
+		printf("절대값은 %i 입니다.\n",i);
+		
+	else if(i<0)
+		printf("절대값은 %i 입니다.\n",i= i*-1);
+		 
+	return 0;
+}
